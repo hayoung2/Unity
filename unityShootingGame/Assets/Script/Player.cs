62cbb396dd67b4fb34b9483fe74d7e6be9807f46
hayoung2 <39898938+hayoung2@users.noreply.github.com>
2019-06-11 21:16:36 +0900

    shootinggame
    unity, c#

---
+++ unityShootingGame/Assets/Script/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (PlayerController))]
+[RequireComponent (typeof (GunController))]
+public class Player : LivingEntity
+{
+    public float moveSpeed = 5;
+
+
+    Camera viewCamera;
+    PlayerController controller;
+
+    GunController gunController;
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        controller = GetComponent<PlayerController>();
+        gunController = GetComponent<GunController>();
+        viewCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //이동을 입력받는곳
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+        controller.Move(moveVelocity);
+        //바라보는곳
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if(groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            //Debug.DrawLine(ray.origin, point, Color.red);
+            controller.LookAt(point);
+        }
+        //무기조작 입력
+        if (Input.GetMouseButton(0))
+        {
+            gunController.Shoot();
+        }
+    }
+}
---
+++ unityShootingGame/Assets/Script/Projectile.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    public LayerMask collisionMask;
+    float speed = 10;
+    float damage = 1;
+    float lifetime = 3;
+    float skinWidth = .1f;
+
+    private void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    public void SetSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+
+        Collider[] InitialCollisions = Physics.OverlapSphere(transform.position, .1f, collisionMask);
+        if (InitialCollisions.Length >0)
+        {
+            OnHItObject(InitialCollisions[0]);
+        }
+    }
+
+    void Update()
+    {
+        float moveDistance = speed * Time.deltaTime;
+        CheckCollisions(moveDistance);
+        transform.Translate(Vector3.forward * moveDistance);
+    }
+
+
+    void CheckCollisions(float moveDistance)
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, moveDistance+skinWidth, collisionMask, QueryTriggerInteraction.Collide))
+        {
+            OnHitObject(hit);
+        }
+    }
+
+    void OnHItObject(Collider c)
+    {
+        IDamageable damageableObject = c.GetComponent<IDamageable>();
+        if (damageableObject != null)
+        {
+            damageableObject.TakeDamage(damage);
+
+        }
+        GameObject.Destroy(gameObject);
+    }
+    void OnHitObject(RaycastHit hit)
+    {
+        IDamageable damageableObject = hit.collider.GetComponent<IDamageable>();
+        if(damageableObject != null)
+        {
+            damageableObject.TakeHit(damage, hit);
+
+        }
+        GameObject.Destroy(gameObject);
+    }
+
+}
