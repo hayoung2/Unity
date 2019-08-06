9fc33ce4bc35b481e4c4a2f5dede84e19230b4da
hayoung2 <39898938+hayoung2@users.noreply.github.com>
2019-08-06 16:53:46 +0900

    2048

--- Academy/2048_/Assets/Scripts/game_manage.cs
+++ Academy/2048_/Assets/Scripts/game_manage.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class game_manage : MonoBehaviour
 {
     public GameObject[] box_base;
-    bool move;
+    bool move=false;
     public float[] x_pos = { -21f, 3f, 27f, 51f };
     public float[] y_pos = { 144f, 120f, 96f, 72f };
     GameObject[,] box = new GameObject[4, 4];
@@ -21,21 +21,45 @@ public class game_manage : MonoBehaviour
         Spawn_box();
 
     }
+    
 
     // Update is called once per frame
     void Update()
     {
+
+
+
         if (Input.GetKey(KeyCode.UpArrow))
             for (x = 0; x < 4; x++)
                 for (y = 3; y > 1; y--)
                     for (int i = 1; i <= y; i++)
-                        combine_move(x, i, x, i-1);
-        else if(Input.GetKey(KeyCode.DownArrow))
+                        combine_move(x, i, x, i - 1);
+        else if (Input.GetKey(KeyCode.DownArrow))
+            for (x = 0; x < 4; x++)
+                for (y = 3; y > 1; y--)
+                    for (int i = 0; i < y; i++)
+                        combine_move(x, i, x, i + 1);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            for (y = 0; y < 4; y++)
+                for (x = 3; x > 1; x--)
+                    for (int i = 0; i < x; i++)
+                        combine_move(i, y, i + 1, y);
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            for (y = 0; y < 4; y++)
+                for (x = 3; x > 1; x--)
+                    for (int i = 1; i <= x; i++)
+                        combine_move(i, y, i - 1, y);
+
     }
 
 
 
+    //IEnumerator boxCreate()
+    //{
 
+    //    Spawn_box();
+    //    yield return new WaitForSeconds(1f);
+    //}
 
 
 
@@ -47,7 +71,7 @@ public class game_manage : MonoBehaviour
         }
         //생성칸에 2또는 4 객체 생성 
         box[x, y] = Instantiate(Random.Range(0, 2) > 0 ? box_base[0] : box_base[1], new Vector3(x_pos[x], y_pos[y], 0), Quaternion.identity);
-
+        
     }
 
     void combine_move(int x_start, int y_start, int x_des, int y_des)
@@ -66,10 +90,9 @@ public class game_manage : MonoBehaviour
             {
 
             }
-
-
         }
-    }
-
+       
+        
+     }
 }
 
