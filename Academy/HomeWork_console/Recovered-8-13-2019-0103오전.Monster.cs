3b6d30bbb503fd31cfc09bdf5338be6209fc9566
hayoung2 <39898938+hayoung2@users.noreply.github.com>
2019-08-14 00:50:31 +0900

    .

---
+++ Academy/HomeWork_console/Recovered-8-13-2019-0103오전.Monster.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Monster
+{
+    public string name;
+    public int hp;
+    public int attack;
+    public int maxhp;
+   
+
+    public Monster(string name, int hp , int attack)
+    {
+        this.name = name;
+        this.hp = hp;
+        this.attack = attack;
+        this.maxhp = hp;
+    }
+
+    public bool Heal()
+    {
+        hp = maxhp;
+        return false;
+    }
+    
+    public bool Attack(int atk)
+    {
+        this.hp -= atk;
+        return false;
+    }
+   
+
+}
---
+++ Academy/HomeWork_console/Recovered-8-13-2019-0103오전.Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Monster> theMob = new List<Monster>();
+        Random rnd = new Random();
+        bool isPrev = false;
+        bool isNext = false;
+        string input="start";
+        int num = rnd.Next(2, 10); 
+
+        Console.WriteLine("\t선수 소개합니다 !");
+
+        for(int i=0; i < (num % 2 == 0 ? num : num+1); i++)
+        {
+            theMob.Add(new Monster((i+1).ToString(), rnd.Next(1, 20), rnd.Next(1, 5)));
+            Console.WriteLine("Name : ",theMob[i].name," HP :", theMob[i].hp," Attack : ", theMob[i].attack);
+
+            if (i == num)
+                num += 1;
+        }
+
+        while (input.Equals("Exit"))
+        {
+            for (int i = 0; ; i++) {
+
+                Console.WriteLine(" 첫번째 상대를 골라주세요 ");
+                string player1 = Console.ReadLine();
+                Console.WriteLine(" 두번째 상대를 골라주세요 ");
+                string player2 = Console.ReadLine();
+
+                while(isPrev || isNext)
+                {
+                    if (isPrev)
+                    {
+
+                    }
+                }
+
+            }
+            
+        }
+        
+
+
+
+        
+
+
+        
+
+
+    }
+}
+
